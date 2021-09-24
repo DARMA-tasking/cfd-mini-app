d4041c994f421a1155b328b29845251edca64b00
Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>
2021-09-24 20:56:35 +0200

    #34: changed default configuration used for testing
    Signed-off-by: Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>

--- cppfd/test.cpp
+++ cppfd/test.cpp
@@ -14,10 +14,10 @@ int main(int argc, char** argv) {
   // define input parameters
   double density = 1.225;
   double dynamic_viscosity = 0.3;
-  double delta_t = 0.001;
+  double delta_t = 0.0001;
   double t_final = 0.1;
   double max_C = 0.5;
-  uint64_t n_cells = 10;
+  uint64_t n_cells = 30;
 
   // create mesh
   Mesh mesh(n_cells, n_cells, 1. / n_cells);
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
 
   // run numerical scheme
   Solver solver(mesh, b_c, delta_t, t_final, density, dynamic_viscosity, max_C, 1);
-  solver.solve(Solver::stopping_point::NONE, Solver::linear_solver::GAUSS_SEIDEL, Solver::adaptative_time_step::ON);
+  solver.solve(Solver::stopping_point::NONE, Solver::linear_solver::CONJUGATE_GRADIENT, Solver::adaptative_time_step::ON);
 
   // save results
   mesh.write_vtk("test.vti");
