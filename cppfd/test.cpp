597d8c6601b2c4e046225bed6fa5b8910f48780d
Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>
2021-09-27 20:38:33 +0200

    #34: updated default test parameters

--- cppfd/test.cpp
+++ cppfd/test.cpp
@@ -14,10 +14,10 @@ int main(int argc, char** argv) {
   // define input parameters
   double density = 1.225;
   double dynamic_viscosity = 0.3;
-  double delta_t = 0.0001;
+  double delta_t = 0.001;
   double t_final = 0.1;
   double max_C = 0.5;
-  uint64_t n_cells = 30;
+  uint64_t n_cells = 4;
   std::cout << "Input parameters:"
 	    << "\n  density: " << density
 	    << "\n  dynamic_viscosity: " << dynamic_viscosity
@@ -46,7 +46,8 @@ int main(int argc, char** argv) {
 
   // run numerical scheme
   Solver solver(mesh, b_c, delta_t, t_final, density, dynamic_viscosity, max_C, 1);
-  solver.solve(Solver::stopping_point::NONE, Solver::linear_solver::GAUSS_SEIDEL, Solver::adaptative_time_step::ON);
+  solver.solve(Solver::stopping_point::NONE, Solver::linear_solver::CONJUGATE_GRADIENT, Solver::adaptative_time_step::ON);
+  //solver.solve(Solver::stopping_point::NONE, Solver::linear_solver::GAUSS_SEIDEL, Solver::adaptative_time_step::ON);
 
   // save results
   mesh.write_vtk("test.vti");
